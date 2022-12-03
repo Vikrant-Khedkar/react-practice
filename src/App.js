8a96dd17f0141af5b28964419ab52ed76b8bf761
vikrant-khedkar <vikrantkhedkar2720@gmail.com>
2022-12-03 23:32:30 +0530

    completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Navbar from './Navbar'
+import Herosection  from './HeroSection';
+import Appsection from './AppSection';
+import Cardsection from './CardsSection';
+import Footer from './FooterSection';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    return(
+        <div>
+        <Navbar/>
+        <Herosection/>
+        <Appsection/>
+        <Cardsection/>
+        <Footer/>
+        </div>
+    );
+};
 
 export default App;
